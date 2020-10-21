f1515289189d781675a8d0cbd20d8a85fefe5423
Kaique F. Souza <kaique.ferr@gmail.com>
2020-10-20 21:53:39 -0300

    ok

---
+++ ww.js
@@ -0,0 +1,32 @@
+function Word(pt,en,song)
+{
+	this.pt = pt;
+	this.en = en;
+	this.song = song;
+}
+let ww = [];
+
+ww.push(new Word("realmente","actually","actually.mp3"));
+ww.push(new Word("de novo","again","again.mp3"));
+ww.push(new Word("foi","been","been.mp3"));
+ww.push(new Word("pertencer","belong","belong.mp3"));
+ww.push(new Word("outro","else","else.mp3"));
+ww.push(new Word("suficiente","enough","enough.mp3"));
+ww.push(new Word("ir","gonna","gonna.mp3"));
+ww.push(new Word("acho","guess","guess.mp3"));
+ww.push(new Word("tipo","kind","kind.mp3"));
+ww.push(new Word("menos","less","less.mp3"));
+ww.push(new Word("importam","matter","matter.mp3"));
+ww.push(new Word("significar","mean","mean.mp3"));
+ww.push(new Word("erro","mistake","mistake.mp3"));
+ww.push(new Word("deveria","should","should.mp3"));
+ww.push(new Word("doente","sick","sick.mp3"));
+ww.push(new Word("ainda","still","still.mp3"));
+ww.push(new Word("pensamento","thought","thought.mp3"));
+ww.push(new Word("contou","told","told.mp3"));
+ww.push(new Word("confiar","trust","trust.mp3"));
+ww.push(new Word("esperar","wait","wait.mp3"));
+ww.push(new Word("quero","wanna","wanna.mp3"));
+ww.push(new Word("quando","when","when.mp3"));
+ww.push(new Word("onde","where","where.mp3"));
+ww.push(new Word("qual","which","which.mp3"));
